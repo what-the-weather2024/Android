87905d20577ac5e6718812038b48cd11e96dbce4
Garam Im <ijy918@naver.com>
2024-08-25 04:12:17 +0900

    feat : My Page 화면 구현

--- app/src/main/java/com/wtw/whattheweather/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/wtw/whattheweather/ui/mypage/MyPageFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.wtw.whattheweather.R
 import com.wtw.whattheweather.databinding.FragmentMyPageBinding
 
@@ -19,7 +20,8 @@ class MyPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_my_page,container,false)
+        binding.lifecycleOwner = viewLifecycleOwner
 
         return binding.root
     }
---
+++ app/src/main/java/com/wtw/whattheweather/ui/mypage/MyPageViewModel.kt
@@ -0,0 +1,6 @@
+package com.wtw.whattheweather.ui.mypage
+
+import androidx.lifecycle.ViewModel
+
+class MyPageViewModel: ViewModel(){
+}
